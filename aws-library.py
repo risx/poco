a4a42c1d17feab8c19d6ace27207dbf50a727f80
risx <rrrisx@gmail.com>
2018-09-04 22:58:20 -0700

    script restructure

--- aws-library.py
+++ aws-library.py
@@ -6,26 +6,20 @@ def FindInstance(tag, value):
     log.info('Running: FindInstance')
 
     ec2 = boto3.client('ec2')
-    res = ec2.describe_instances(
-    Filters=[{'Name': ('tag:'+tag),'Values': [value]}])
+    res = ec2.describe_instances(Filters=[{'Name': ('tag:'+tag),'Values': [value]}])
 
     try:
-        iID = res['Reservations'][0]['Instances'][0]['InstanceId']
-        iPIP = res['Reservations'][0]['Instances'][0]['PrivateIpAddress']
-        iIP = res['Reservations'][0]['Instances'][0]['PublicIpAddress']
-        iState = res['Reservations'][0]['Instances'][0]['State']['Name']
+        instanceinfo = {
+            'InstanceID': res['Reservations'][0]['Instances'][0]['InstanceId'],
+            'InstancePrivateIP': res['Reservations'][0]['Instances'][0]['PrivateIpAddress'],
+            'InstanceIP': res['Reservations'][0]['Instances'][0]['PublicIpAddress'],
+            'InstanceState': res['Reservations'][0]['Instances'][0]['State']['Name']
+        }
 
     except IndexError as e:
         log.info('Cannot Find Instance')
         return False
 
-    instanceinfo = {
-        'InstanceID': iID,
-        'InstancePrivateIP': iPIP,
-        'InstanceIP': iIP,
-        'InstanceState': iState
-    }
-    
     return instanceinfo
     
 
@@ -34,7 +28,7 @@ def main():
 
 if __name__ == '__main__':
     #Logging Setup
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(name)s | LOG: %(message)s')
+    logging.basicConfig(level=20, format='%(asctime)s: %(name)s | LOG: %(message)s')
     log = logging.getLogger(__name__)
 
     main()
