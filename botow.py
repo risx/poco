97c5b701e629a73109088a48c17a2522ab5565fa
risx <rrrisx@gmail.com>
2018-10-06 21:04:51 -0700

    factored to classes

--- botow.py
+++ botow.py
@@ -5,57 +5,64 @@ import logging
 logging.basicConfig(level=20, format='%(asctime)s: %(name)s | LOG: %(message)s')
 log = logging.getLogger(__name__)
 
-#Takes in a tag and a value to search for and returns an dictionary of information
-#about the instance: InstanceID, PriviateIP, PublicIP, InstanceState
-def FindInstance(tag, value):
-    log.info('Running: FindInstance', tag, value)
-
-    ec2 = boto3.client('ec2')
-    res = ec2.describe_instances(Filters=[{'Name': ('tag:'+tag),'Values': [value]}])
-
-    try:
-        instance_state = res['Reservations'][0]['Instances'][0]['State']['Name']
-
-        try:
-            instance_id = res['Reservations'][0]['Instances'][0]['InstanceId']
-        except:
-            instance_id = 'null'
-        try:
-            private_ip = res['Reservations'][0]['Instances'][0]['PrivateIpAddress']
-        except:
-            private_ip = 'null'
-        try:
-            public_ip = res['Reservations'][0]['Instances'][0]['PublicIpAddress']
-        except:
-            public_ip = 'null'
-
-        instanceinfo = {
-            'InstanceID': instance_id,
-            'InstancePrivateIP': private_ip,
-            'InstanceIP': public_ip,
-            'InstanceState': instance_state
-        }
-
-    except IndexError as e:
-        log.info('Cannot Find Instance')
-        return False
-
-    return instanceinfo
+class ParameterHandler:
+    def __init__(self, tag, value):
+        self.tag = tag
+        self.value = value
     
+    def find(self):
+        log.info('Running: FindSSMParam')
 
-def FindSSMParam(tag, value):
-    log.info('Running: FindSSMParam')
+        ssm = boto3.client('ssm')
+        res = ssm.describe_parameters(Filters=[{'Key': (self.tag),'Values': [self.value]}])
 
-    ssm = boto3.client('ssm')
-    res = ssm.describe_parameters(Filters=[{'Key': (tag),'Values': [value]}])
+        try:
+            ssm_info = {
+                'Name': res['Parameters'][0]['Name'],
+                #'Value': res['Paremeters'][0][]
+            }
+        except IndexError as e:
+            log.info('Cannot Find Parameter')
+            return False
 
-    try:
-        ssm_info = {
-            'Name': res['Parameters'][0]['Name'],
-            'Value': res['Paremeters'][0][]
-        }
-    except IndexError as e:
-        log.info('Cannot Find Parameter')
-        return False
+        return ssm_info
 
-    return ssm_info
+class InstanceHandler:
+    def __init__(self, tag, value):
+        self.tag = tag
+        self.value = value
+
+    def find(self):
+        log.info('Running: findInstance', self.tag, self.value)
+
+        ec2 = boto3.client('ec2')
+        res = ec2.describe_instances(Filters=[{'Name': ('tag:'+self.tag),'Values': [self.value]}])
+
+        try:
+            instance_state = res['Reservations'][0]['Instances'][0]['State']['Name']
+
+            try:
+                instance_id = res['Reservations'][0]['Instances'][0]['InstanceId']
+            except:
+                instance_id = 'null'
+            try:
+                private_ip = res['Reservations'][0]['Instances'][0]['PrivateIpAddress']
+            except:
+                private_ip = 'null'
+            try:
+                public_ip = res['Reservations'][0]['Instances'][0]['PublicIpAddress']
+            except:
+                public_ip = 'null'
+
+            instanceinfo = {
+                'InstanceID': instance_id,
+                'InstancePrivateIP': private_ip,
+                'InstanceIP': public_ip,
+                'InstanceState': instance_state
+            }
+
+        except IndexError as e:
+            log.info('Cannot Find Instance')
+            return False
+
+        return instanceinfo
--- discord-bot.py
+++ discord-bot.py
@@ -19,8 +19,10 @@ async def on_message(message):
         tag_name = message.content.split()[1]
         value_name = message.content.split()[2]
 
-        if (botow.FindInstance(tag_name, value_name)):
-            instance_dic = botow.FindInstance(tag_name,value_name)
+        instance = botow.InstanceHandler(tag_name, value_name)
+
+        if (instance.find()):
+            instance_dic = instance.find()
 
             em = discord.Embed(title='Instance Information', description='Instance Found', colour=0x00ff00)
             em.add_field(name='Instance State', value=str(instance_dic['InstanceState']), inline=False)
@@ -37,8 +39,10 @@ async def on_message(message):
         tag_name = message.content.split()[1]
         value_name = message.content.split()[2]
 
-        if (botow.FindSSMParam(tag_name, value_name)):
-            ssm_dic = botow.FindSSMParam(tag_name, value_name)
+        parameter = botow.ParameterHandler(tag_name, value_name)
+
+        if (parameter.find()):
+            ssm_dic = parameter.find()
 
             em = discord.Embed(title='Paramter Information', description='Parameter Found!', colour=0x00ff00)
             em.add_field(name='Parameter Value', value=str(ssm_dic), inline=False)
